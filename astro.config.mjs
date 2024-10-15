1df2e2366e42e711faa6424ba0f833e0d2edb77d
Lan Yang Villavicencio <leelanzing@hotmail.com>
2024-10-15 11:24:15 -0300

    Revert "upload vercel analytics"
    This reverts commit d03abf9b02a40f85d87c530241232df4d7e41b89.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-import vercel from '@astrojs/vercel/serverless';
 
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://lanyangporfolio-dev.vercel.app',
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+  site: 'https://porfolio.dev/'
 })
