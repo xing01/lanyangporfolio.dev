d03abf9b02a40f85d87c530241232df4d7e41b89
Lan Yang Villavicencio <leelanzing@hotmail.com>
2024-10-15 11:02:41 -0300

    upload vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
+import vercel from '@astrojs/vercel/serverless';
 
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: 'https://lanyangporfolio-dev.vercel.app',
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 })
